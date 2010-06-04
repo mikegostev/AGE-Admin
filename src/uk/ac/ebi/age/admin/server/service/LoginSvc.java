f91e4b18073173f29bfa3161b13dd23d04eb6cfd
Mike <gostev@ebi.ac.uk>
2010-06-04 08:16:25 +0100

    Day of speech

---
+++ src/uk/ac/ebi/age/admin/server/service/LoginSvc.java
@@ -0,0 +1,39 @@
+package uk.ac.ebi.age.admin.server.service;
+
+import java.io.IOException;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import uk.ac.ebi.age.admin.client.common.Constants;
+import uk.ac.ebi.age.admin.server.mng.AgeAdmin;
+
+public class LoginSvc extends HttpServlet
+{
+
+ private static final long serialVersionUID = 1L;
+
+ protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException
+ {
+  String uname = req.getParameter("username");
+  String pass = req.getParameter("password");
+  
+  try
+  {
+   String sessId =  AgeAdmin.getDefaultInstance().login(uname, pass, req.getRemoteAddr()).getSessionKey();
+   
+   resp.addCookie( new Cookie(Constants.sessionCookieName, sessId) );
+   
+  }
+  catch (Exception e) 
+  {
+   resp.getWriter().println("Failed");
+   return;
+  }
+  
+  resp.getWriter().println("OK");
+
+ }
+}
