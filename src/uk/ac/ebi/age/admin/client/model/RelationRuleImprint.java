0e937503a6d9a170ae50373016a176a8e164aa2c
mike <mike@MiGee>
2010-10-05 17:35:37 +0100

    Rule imprints update

--- src/uk/ac/ebi/age/admin/client/model/RelationRuleImprint.java
+++ src/uk/ac/ebi/age/admin/client/model/RelationRuleImprint.java
@@ -3,8 +3,6 @@ package uk.ac.ebi.age.admin.client.model;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Map;
-import java.util.TreeMap;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
@@ -20,9 +18,8 @@ public class RelationRuleImprint implements Serializable, IsSerializable
  private AgeRelationClassImprint relationClass;
  
  private int cardinality=1;
- private Map<RestrictionType,Collection<QualifierRuleImprint>> qualifiers;
+ private Collection<QualifierRuleImprint> qualifiers;
  
- private boolean qualifiersUnique;
  private boolean subclassesIncluded=true;
  private boolean relSubclassesIncluded=true;
 
@@ -69,41 +66,6 @@ public class RelationRuleImprint implements Serializable, IsSerializable
   this.cardinality = cardinality;
  }
 
- public Map<RestrictionType,Collection<QualifierRuleImprint>> getQualifiersMap()
- {
-  return qualifiers;
- }
-
-// public Collection<QualifierRuleImprint> getQualifiers()
-// {
-//  if( qualifiers == null )
-//   return null;
-//  
-//  return new CollectionsUnion<QualifierRuleImprint>( qualifiers.values() );
-// }
- 
- public void addQualifier( QualifierRuleImprint qr )
- {
-  if( qualifiers == null )
-  {
-   qualifiers=new TreeMap<RestrictionType, Collection<QualifierRuleImprint>>();
-   Collection<QualifierRuleImprint> coll = new ArrayList<QualifierRuleImprint>(5);
-   coll.add(qr);
-   qualifiers.put(qr.getType(), coll);
-   return;
-  }
-  
-  Collection<QualifierRuleImprint> coll = qualifiers.get(qr.getType());
-  
-  if( coll == null )
-  {
-   coll = new ArrayList<QualifierRuleImprint>(5);
-   qualifiers.put(qr.getType(), coll);
-  }
- 
-  coll.add(qr);
- 
- }
  
  public void setTargetClass(AgeClassImprint tClass)
  {
@@ -114,6 +76,8 @@ public class RelationRuleImprint implements Serializable, IsSerializable
  {
   StringBuilder sb = new StringBuilder();
   
+  boolean singleRel = cardinality == 1 && cardType != Cardinality.MIN;
+
   sb.append("object ").append(type.getTitle()).append(" have ");
   
   
@@ -122,144 +86,102 @@ public class RelationRuleImprint implements Serializable, IsSerializable
    if( cardType == Cardinality.ANY)
     sb.append(" any number of relations ");
    else
-    sb.append(cardType.getTitle()).append(" ").append(cardinality).append(" relation").append(cardinality>1?"s ":" ");
+    sb.append(cardType.getTitle()).append(" ").append(cardinality).append(" relation").append(singleRel?" ":"s ");
    
-   if( ( cardinality > 1 || cardType == Cardinality.MIN ) &&  qualifiersUnique )
-   {
-    sb.append("(all ");
-    
-    if(qualifiersUnique)
-     sb.append("qualifiers");
-    
-    sb.append(" must be unique) ");
-    
-   }
+//   if( ( cardinality > 1 || cardType == Cardinality.MIN ) &&  qualifiersUnique )
+//   {
+//    sb.append("(all ");
+//    
+//    if(qualifiersUnique)
+//     sb.append("qualifiers");
+//    
+//    sb.append(" must be unique) ");
+//    
+//   }
    
-   sb.append("with class <b>").append(targetClass.getName()).append("</b> (");
+   sb.append("of class <b>").append(targetClass.getName()).append("</b> (");
    
    if( subclassesIncluded )
     sb.append("including subclasses) ");
    else
     sb.append("excluding subclasses) ");
    
-   if( qualifiers != null )
-   {
-    boolean hasQ=false;
-  
-    sb.append("and ");
-    
-    for( RestrictionType rt : RestrictionType.values() )
-    {
-     Collection<QualifierRuleImprint> qcoll = qualifiers.get(rt);
-     
-     if( qcoll != null && qcoll.size() > 0 )
-     {
-      hasQ=true;
-      sb.append(rt.getTitle()).append(" have qualifiers of class");
-      
-      if( qcoll.size() > 1 )
-       sb.append("es");
-      
-      sb.append(" ");
-      
-      for( QualifierRuleImprint qr : qcoll )
-       sb.append("<b>").append(qr.getAttributeClassImprint().getName()).append("</b>, ");
-      
-      sb.setLength( sb.length()-2 );
-      
-      sb.append(" and ");
-     }
-    }
-
-    if( hasQ )
-     sb.setLength( sb.length()-5 );
-    else
-     sb.setLength( sb.length()-6 );
-   }
   }
   else
   {
    if( cardType != Cardinality.ANY )
    {
-    sb.append(" if they have ");
+    sb.append("relations if they have ");
     
     switch(cardType)
     {
      case EXACT:
-      sb.append(cardinality).append(" values");
+      sb.append("exactly ").append(cardinality);
       break;
  
      case MIN:
-      sb.append(cardinality).append(" or more values");
+      sb.append(cardinality).append(" or more");
       break;
 
      case MAX:
-      sb.append(cardinality).append(" or less values");
+      sb.append(cardinality).append(" or less");
       break;
 
      default:
       break;
     }
+    
+    sb.append(" relation");
+    
+    if( ! singleRel )
+     sb.append('s');
    }
    
-   if( qualifiersCondition != QualifiersCondition.ANY )
-   {
-    if( cardType != Cardinality.ANY )
-     sb.append(" and");
-    
-    if( qualifiersCondition == QualifiersCondition.NONE )
-     sb.append(" if they have no qualifiers");
-    else if( qualifiers != null && qualifiers.size() > 0 )
-    {
-     sb.append(" if they have the following qualifier");
-     
-     sb.append( qualifiers.size() > 1?"s ":" ");
-     
-     for( QualifierRuleImprint qr : qualifiers.get(RestrictionType.MUST) )
-      sb.append("<b>").append(qr.getAttributeClassImprint().getName()).append("</b>, ");
-
-     sb.setLength(sb.length()-2);
-    }
-   }
   }
   
-  sb.append(" with class <b>").append(targetClass.getName()).append("</b> (");
+  sb.append(" with objects of class <b>").append(targetClass.getName()).append("</b> (");
   
   if( subclassesIncluded )
    sb.append("including subclasses)");
   else
    sb.append("excluding subclasses)");
 
+  sb.append('.');
   
+
+  if( qualifiers != null && qualifiers.size() > 0 )
+  {
+ 
+   sb.append(" Rule is applied only if relation");
+   if( !singleRel )
+    sb.append("s have");
+   else
+    sb.append(" has");
+   
+   sb.append(" qualifier(s) of class");
+   
+   if( qualifiers.size() > 1 )
+    sb.append("es");
+    
+   sb.append(" ");
+    
+   for( QualifierRuleImprint qr : qualifiers )
+   {
+    sb.append("<b>").append(qr.getAttributeClassImprint().getName()).append("</b>");
+    
+    if(  qr.isUnique() )
+     sb.append(" (unique)");
+
+     sb.append(", ");
+   }
+
+   sb.setLength( sb.length()-2 );
+  }
+  
+  sb.append('.');
 
   
   return sb.toString();
- }
-
- public void clearQualifiers()
- {
-  if( qualifiers != null )
-   qualifiers.clear();
- }
-
- public boolean isQualifiersUnique()
- {
-  return qualifiersUnique;
- }
-
- public void setQualifiersUnique(boolean qualifiersUnique)
- {
-  this.qualifiersUnique = qualifiersUnique;
- }
-
- public void setQualifiersCondition(QualifiersCondition qc)
- {
-  qualifiersCondition = qc;
- }
-
- public QualifiersCondition getQualifiersCondition()
- {
-  return qualifiersCondition;
  }
 
  public boolean isSubclassesIncluded()
@@ -317,6 +239,23 @@ public class RelationRuleImprint implements Serializable, IsSerializable
   this.id = id;
  }
 
+ public Collection<QualifierRuleImprint> getQualifiers()
+ {
+  return qualifiers;
+ }
 
+ public void addQualifier( QualifierRuleImprint qr )
+ {
+  if( qualifiers == null )
+   qualifiers=new ArrayList<QualifierRuleImprint>();
+
+  qualifiers.add(qr);
+ }
+
+ public void clearQualifiers()
+ {
+  if( qualifiers != null )
+   qualifiers.clear();
+ }
 
 }
