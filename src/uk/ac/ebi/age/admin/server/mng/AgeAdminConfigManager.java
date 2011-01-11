77a645d0d030b92892f0e689f88ec8e0b9ef4164
Mike <Mike@172.22.68.168>
2011-01-11 17:02:59 +0000

    Separate Admin launch

---
+++ src/uk/ac/ebi/age/admin/server/mng/AgeAdminConfigManager.java
@@ -0,0 +1,103 @@
+package uk.ac.ebi.age.admin.server.mng;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+
+
+public class AgeAdminConfigManager
+{
+ public static final String BASE_PATH_PARAM="basePath";
+ public static final String DB_PATH_PARAM="dbPath";
+ public static final String TMP_PATH_PARAM="tmpPath";
+ public static final String SERVICES_PATH_PARAM="servicesPath";
+ 
+ @SuppressWarnings("serial")
+ private Map<String,String> configMap = new HashMap<String,String>(){{
+  put(BASE_PATH_PARAM,      "var/biosd/");
+  put(DB_PATH_PARAM,        "agedb");
+  put(TMP_PATH_PARAM,       "tmp");
+  put(SERVICES_PATH_PARAM,  "services");
+ }};
+ 
+// private static AgeAdminConfigManager instance = null;
+ 
+ public AgeAdminConfigManager(ServletContext servletContext)
+ {
+  Enumeration<?> pNames = servletContext.getInitParameterNames();
+  
+  while( pNames.hasMoreElements() )
+  {
+   String key = pNames.nextElement().toString();
+   configMap.put(key, servletContext.getInitParameter(key) );
+  }
+ }
+ 
+// public static void setInstance( AgeAdminConfigManager inst )
+// {
+//  instance=inst;
+// }
+//
+// 
+// public static AgeAdminConfigManager instance()
+// {
+//  return instance;
+// }
+
+ public String getBasePath()
+ {
+  return getConfigParameter(BASE_PATH_PARAM);
+ }
+
+ 
+ public String getDBPath()
+ {
+  return getPathParam(DB_PATH_PARAM);
+ }
+
+ public String getTmpPath()
+ {
+  return getPathParam(TMP_PATH_PARAM);
+ }
+
+ public String getServicesPath()
+ {
+  return getPathParam(SERVICES_PATH_PARAM);
+ }
+
+ public String getPathParam( String key )
+ {
+  String basePath = getBasePath();
+  String path = getConfigParameter(key);
+  
+  if( path == null )
+   path=key;
+  
+  if( basePath.endsWith("/") || path.startsWith("/") )
+   return basePath+path;
+
+  return basePath+"/"+path;
+ }
+
+ 
+ public String getConfigParameter( String key )
+ {
+  return configMap.get(key);
+ }
+
+ public String setConfigParameter( String key, String value )
+ {
+  String old = configMap.get(key);
+  configMap.put(key, value);
+  return old;
+ }
+
+ public boolean isMaster()
+ {
+  String mval = configMap.get("isMaster");
+ 
+  return mval != null && ( "true".equalsIgnoreCase(mval) || "yes".equalsIgnoreCase(mval) || "1".equalsIgnoreCase(mval) ); 
+ }
+}
--- src/uk/ac/ebi/age/admin/server/service/Init.java
+++ src/uk/ac/ebi/age/admin/server/service/Init.java
@@ -1,30 +1,60 @@
 package uk.ac.ebi.age.admin.server.service;
 
+import java.io.File;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import uk.ac.ebi.age.admin.server.mng.AgeAdmin;
+import uk.ac.ebi.age.admin.server.mng.AgeAdminConfigManager;
 import uk.ac.ebi.age.admin.server.mng.Configuration;
-import uk.ac.ebi.age.admin.server.mng.UploadManager;
-import uk.ac.ebi.age.admin.server.user.impl.SessionPoolImpl;
-import uk.ac.ebi.age.admin.server.user.impl.TestUserDataBase;
+import uk.ac.ebi.age.mng.AgeStorageManager;
+import uk.ac.ebi.age.mng.AgeStorageManager.DB_TYPE;
+import uk.ac.ebi.age.service.IdGenerator;
+import uk.ac.ebi.age.service.impl.SeqIdGeneratorImpl;
+import uk.ac.ebi.age.storage.AgeStorageAdm;
 
 public class Init implements ServletContextListener
 {
-
+ AgeAdmin adm ;
+ 
  @Override
  public void contextDestroyed(ServletContextEvent arg0)
  {
-  Configuration.getDefaultConfiguration().getSessionPool().shutdown();
+  if( adm != null )
+   adm.shutdown();
  }
 
  @Override
  public void contextInitialized(ServletContextEvent arg0)
  {
+  AgeAdminConfigManager cfg = new AgeAdminConfigManager(arg0.getServletContext());
+  AgeStorageAdm storage;
+  
+  try
+  {
+   boolean master = cfg.isMaster();
+   
+   if( master )
+    IdGenerator.setInstance( new SeqIdGeneratorImpl(cfg.getServicesPath()+"/SeqIdGen") );
+   
+   storage = AgeStorageManager.createInstance( DB_TYPE.AgeDB, cfg.getDBPath(), master );
+  }
+  catch(Exception e)
+  {
+   e.printStackTrace();
+   return;
+  }
+  
   Configuration conf = Configuration.getDefaultConfiguration();
   
-  conf.setSessionPool(new SessionPoolImpl() );
-  conf.setUserDatabase( new TestUserDataBase() );
-  conf.setUploadManager( new UploadManager() );
+  conf.setBaseDir( new File(cfg.getBasePath()) );
+  conf.setTmpDir( new File(cfg.getTmpPath()) );
+
+  conf.getPublicModelDir().mkdirs();
+  conf.getUserBaseDir().mkdirs();
+  
+  adm = new AgeAdmin(conf, storage);
  }
 
 }
